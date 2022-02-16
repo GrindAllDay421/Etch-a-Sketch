7f070a6bb149d5bbd22a6641d0fc32a27e3cc416
GrindAllDay421 <codiejaworsky@gmail.com>
2022-02-15 21:56:38 -0700

    finished basic project requirements

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,8 +1,35 @@
-for(let i = 0; i < (16 * 16); i++) {
-  const makeDiv = document.createElement('div');
-  const hello = document.createTextNode('Hello World!');
-  const container = document.getElementById('container');
+const btnContainer = document.createElement("div");
+const resetBtn = document.createElement("button");
+const btnText = document.createTextNode("Reset");
+const container = document.getElementById('container');
+let gridSize = 16;
+let numberOfColumns;
 
-  makeDiv.appendChild(hello);
-  container.appendChild(makeDiv);
+resetBtn.appendChild(btnText);
+resetBtn.classList.add("resetBtnStyle");
+btnContainer.appendChild(resetBtn);
+btnContainer.classList.add("resetBtnContainer");
+document.body.prepend(btnContainer);
+
+const makeNewGrid = function() {
+  container.style.gridTemplateColumns = `repeat(${gridSize}, auto)`;
+  for(let gridItems = 0; gridItems < gridSize ** 2; gridItems++) {
+    const makeDiv = document.createElement("div");
+    makeDiv.onmouseover = function() {
+      makeDiv.classList.add("hover");
+    }
+    container.appendChild(makeDiv);
+  }
 }
+
+makeNewGrid();
+
+const resetGrid = function() {
+  container.replaceChildren();
+  gridSize = 0;
+  do {
+    gridSize = window.prompt("Enter new grid size from 1-100: ");
+  } while (gridSize < 1 || gridSize > 100);
+  makeNewGrid();
+}
+resetBtn.addEventListener('click', resetGrid);
